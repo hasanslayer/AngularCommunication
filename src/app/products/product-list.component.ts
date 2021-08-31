d6a7a43bcc94fbfb72d23f45c412f56307530bed
Hasan Darwish <hasan-dr2010@hotmail.com>
2021-09-01 00:18:21 +0200

    first commit

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+    pageTitle: string = 'Product List';
+    listFilter: string;
+    showImage: boolean;
+
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    errorMessage: string;
+
+    filteredProducts: IProduct[];
+    products: IProduct[];
+
+    constructor(private productService: ProductService) { }
+
+    ngOnInit(): void {
+        this.productService.getProducts().subscribe(
+            (products: IProduct[]) => {
+                this.products = products;
+                this.performFilter(this.listFilter);
+            },
+            (error: any) => this.errorMessage = <any>error
+        );
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    performFilter(filterBy?: string): void {
+        if (filterBy) {
+            this.filteredProducts = this.products.filter((product: IProduct) =>
+                product.productName.toLocaleLowerCase().indexOf(filterBy.toLocaleLowerCase()) !== -1);
+        } else {
+            this.filteredProducts = this.products;
+        }
+    }
+}
