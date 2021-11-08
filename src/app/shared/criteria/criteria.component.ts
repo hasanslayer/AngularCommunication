9e96a693f7685791be656e1ff125b3d03df4c896
Hasan Darwish <hasan-dr2010@hotmail.com>
2021-11-09 01:47:24 +0200

    Implement onchages event to the child component.

--- src/app/shared/criteria/criteria.component.ts
+++ src/app/shared/criteria/criteria.component.ts
@@ -1,17 +1,26 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'pm-criteria',
   templateUrl: './criteria.component.html',
   styleUrls: ['./criteria.component.css']
 })
-export class CriteriaComponent implements OnInit, AfterViewInit {
+export class CriteriaComponent implements OnInit, OnChanges, AfterViewInit {
   listFilter: string;
   @Input() displayDetail: boolean;
   @Input() hitCount: number;
+  hitMessage: string;
 
   @ViewChild('filterElement') filterElementRef: ElementRef;
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['hitCount'] && !changes['hitCount'].currentValue) {
+      this.hitMessage = 'No matches found';
+    } else {
+      this.hitMessage = 'Hits: ' + this.hitCount;
+    }
+  }
 
   ngAfterViewInit(): void {
     if (this.filterElementRef) {
